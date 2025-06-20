8e4506d8b0bd9ed483340893eae8c1edd5c746ac
Krycha77 <marczakkrystian4@gmail.com>
2025-06-21 00:34:01 +0200

    crud and gui for client form

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkintermapview
 from tkinter import ttk
 from carwash import *
 from employee import *
+from client import *
 
 root = Tk()
 root.geometry("1400x800")
@@ -12,12 +13,15 @@ tabControl = ttk.Notebook(root)
 
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
+tab3 = ttk.Frame(tabControl)
 
 configure_carwash_gui(tab1)
 configure_employee_gui(tab2)
+configure_client_gui(tab3)
 
 tabControl.add(tab1, text ='Myjnie')
 tabControl.add(tab2, text ='Pracownicy')
+tabControl.add(tab3, text ='Klienci')
 tabControl.pack(expand = 1, fill ="both")
 
 root.mainloop()
